d21667ce13fe8a457b101ce3185a75cba25e393c
mcervantes71 <cervantes.martine@gmail.com>
2021-03-26 13:56:24 -0600

    Add basic recursion

---
+++ 02 functional-javascript/07 reducer.js
@@ -0,0 +1,6 @@
+module.exports =  reduce = (arr, fn, initial) => (
+  (reduceOne = (index, value) => {
+    if (index > arr.length - 1) return value
+    return reduceOne(index + 1, fn(value, arr[index], index, arr))
+  })(0, initial)
+)
